9c1f03254f9d683fe5e9ef30b798663d3b105c73
Priyanshu-Batham <priyanshubatham24733@gmail.com>
2023-03-21 20:37:53 +0530

    player class added

--- project.py
+++ project.py
@@ -1,60 +1,6 @@
 import random
-R = [random.randrange(1, 7), random.randrange(1, 7), random.randrange(
-    1, 7), random.randrange(1, 7), random.randrange(1, 7), random.randrange(1, 7)]
-count = [0, 0, 0, 0, 0, 0]
-
-goatColors = ["BLack", "Orange", "Purple", "Green", "Red", "White"]
-playerInfo = []
 
 
-def drawline():
-    for i in range(0, 10):
-        print("+---", end="")
-    print("+\n")
-
-
-while True:
-    n = int(input("Number of players:"))
-    if n > 6 or n <= 1:
-        print("Max players allowed are 6 and Min 2")
-        continue
-    else:
-        break
-for i in range(0, n):
-    print("Enter Player", i+1, "name:")
-    name = input()
-    print(name, "Has", goatColors[i], "Goat")
-    query = [i+1, name, goatColors[i]]
-    playerInfo.append(query)
-
-# visual representation of the game board------------------------------>>>
-
-
-def board():
-    print("Start", end=" ")
-    for j in range(65, 73):
-        print(chr(j), end="   ")
-    print("End")
-    print("\n")
-    for i in range(0, 6):
-        drawline()
-        print(i+1, end="   ")
-        for r in range(0, 10):
-            print("|", end="")
-            if R[i] == r:
-                print(" X ", end="")
-            else:
-                print(end="   ")
-        print("\n")
-    drawline()
-    print("Player Number | Player Name | Goat Colour")
-    for f in playerInfo:
-        for q in f:
-            print("     ", q, end="      ")
-        print("\n")
-
-
-board()
 
 # --------------------------STACK CLASS--------------------->>
 
@@ -88,7 +34,7 @@ print(stack.peek())
 class Goat:
     color = ""
     row = -1
-    column = ''
+    column = '-1'
 
     # setting the color
     def __init__(self, goatColor):
@@ -120,14 +66,111 @@ print(goat1.getLocation())
 # goat class working smoothly
 '''
 
-
-class Player:
-    def __init__(self, playernumber):
-        self.playernumber = playernumber
-        print(playerInfo[playernumber])
-     #   print(I[playernumber[2]])
+# ----------------PLAYER CLASS---------------->>>>>
 
 
-g = int(input("Enter player number:"))
-o = Player(g)
-print(o.playernumber)
+class Player(Goat):
+    color = ''
+    playerName = ''
+    playerGoats = []
+    def __init__(self, playerName, color):
+        self.color = color
+        self.playerName = playerName
+        for i in range(0,4):
+            obj = Goat(color)
+            self.playerGoats.append(obj)
+
+    def __str__(self) -> str:
+        return f"Name: {self.playerName}\t Color: {self.color}\nGoat 1: {self.getGoat(0)}\nGoat 2: {self.getGoat(1)}\nGoat 3: {self.getGoat(2)}\nGoat 4: {self.getGoat(3)}"
+
+    def addGoat(self):
+        obj = Goat(self.color)
+        self.playerGoats.append(obj)
+
+    def removeGoat(self,num):
+        self.playerGoats.pop(num)
+
+    def playerColor(self):
+        return self.color
+    
+    def goatCount(self):
+        return len(self.playerGoats)
+    
+    def setGoat(self,num,column,row):
+        self.playerGoats[num].setLocation(column,row)
+
+    def getGoat(self,num):
+        return self.playerGoats[num].getLocation()
+# ------------------------------------------->>>>>>
+
+R = [random.randrange(1, 7), random.randrange(1, 7), random.randrange(
+    1, 7), random.randrange(1, 7), random.randrange(1, 7), random.randrange(1, 7)]
+count = [0, 0, 0, 0, 0, 0]
+goatColors = ["BLack", "Orange", "Purple", "Green", "Red", "White"]
+players = []
+
+# visual representation of the game board------------------------------>>>
+def drawline():
+    for i in range(0, 10):
+        print("+---", end="")
+    print("+\n")
+
+
+def board():
+    print("Start", end=" ")
+    for j in range(65, 73):
+        print(chr(j), end="   ")
+    print("End")
+    print("\n")
+    for i in range(0, 6):
+        drawline()
+        print(i+1, end="   ")
+        for r in range(0, 10):
+            print("|", end="")
+            if R[i] == r:
+                print(" X ", end="")
+            else:
+                print(end="   ")
+        print("\n")
+    drawline()
+
+
+board()
+    
+
+while True:
+    n = int(input("Number of players:"))
+    if n > 6 or n <= 1:
+        print("Max players allowed are 6 and Min 2")
+        continue
+    else:
+        break
+for i in range(0, n):
+    print("Enter Player", i+1, "name:")
+    name = input()
+    print("\n",name, "Has", goatColors[i], "Goat\n")
+
+    print("Enter 4 Goat Locations\n")
+    goat1column = input()
+    goat1row = int(input())
+
+    goat2column = input()
+    goat2row = int(input())
+
+    goat3column = input()
+    goat3row = int(input())
+
+    goat4column = input()
+    goat4row = int(input())
+
+    playerObj = Player(name,goatColors[i])
+    playerObj.setGoat(0,goat1column,goat1row)
+    playerObj.setGoat(1,goat2column,goat2row)
+    playerObj.setGoat(2,goat3column,goat3row)
+    playerObj.setGoat(3,goat4column,goat4row)
+    players.append(playerObj)
+
+
+for i in range(0,n):
+    print(players[i],"\n")
+
